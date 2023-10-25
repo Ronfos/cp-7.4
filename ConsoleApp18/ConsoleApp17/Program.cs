78f15b539cd8d12e31797a77c3e3bfe1bc8a3c6e
Ronfos <74715764+Ronfos@users.noreply.github.com>
2023-10-25 21:02:36 +0300

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            string s2 = s.ToString();
+            Console.Write("Строка без х: ");
+            foreach (char a in s2)
+            {
+                if (a == 'х') { }
+                else
+                { Console.Write(a); }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
